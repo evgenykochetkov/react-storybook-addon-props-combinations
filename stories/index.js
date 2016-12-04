782e2f434682a9e3108361cbc1f602bf01c70e05
Evgeny Kochetkov <evgeny.kochetkov@me.com>
2016-12-04 20:43:20 +0300

    Rendering props that are React elements a bit nicer

--- stories/index.js
+++ stories/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
+import reactElementToJSXString from 'react-element-to-jsx-string';
 
 import { combinations } from './utils'
 
@@ -8,11 +9,18 @@ const propsCombinations = combinations({
   children: ['', 'hello world', 'some moderately long text', <b>some other elements</b>]
 })
 
+const replacer = (key, value) => {
+  if (React.isValidElement(value)) {
+    return reactElementToJSXString(value)
+  }
+  return value;
+}
+
 const Decorator = ({children, passedProps}) => (
   <div>
     {children}
     <pre>
-      {JSON.stringify(passedProps, null, 2)}
+      {JSON.stringify(passedProps, replacer, 2)}
     </pre>
   </div>
 )
