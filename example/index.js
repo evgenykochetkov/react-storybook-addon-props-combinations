7016372b3aa555796d0747a20a70727690d0eddb
Evgeny Kochetkov <evgeny.kochetkov@me.com>
2016-12-05 22:25:54 +0300

    Add mustProvideAllProps option
    Closes #5

--- example/index.js
+++ example/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { PropTypes as pt } from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 
-storiesOf('Button', module)
+storiesOf('Standard button', module)
   .addWithPropsCombinations(
     'Props Combinations',
     'button',
@@ -11,6 +11,38 @@ storiesOf('Button', module)
       children: ['hello world', <b>some other elements</b>]
     },
     {
-      showSource: false
+      showSource: false,
+    }
+  )
+
+class SomeComponent extends React.Component {
+  static propTypes = {
+    foo: pt.string.isRequired,
+    bar: pt.string.isRequired,
+    baz: pt.bool.isRequired,
+  }
+
+  render () {
+    const { foo, bar, baz } = this.props
+
+    return (
+      <div>
+        {foo} | {bar} | {baz.toString()}
+      </div>
+    )
+  }
+}
+
+storiesOf('SomeComponent', module)
+  .addWithPropsCombinations(
+    'mustProvideAllProps test',
+    SomeComponent,
+    {
+      // foo: [1, 2],
+      bar: ['a', 'b'],
+      baz: [false, true],
+    },
+    {
+      mustProvideAllProps: true,
     }
   )
--- src/index.js
+++ src/index.js
@@ -25,20 +25,35 @@ const defaultRender = (component, props, options) => {
 
 const defaultOptions = {
   render: defaultRender,
-  showSource: true
+  showSource: true,
+  mustProvideAllProps: false,
 }
 
 export default {
   addWithPropsCombinations (storyName, component, possiblePropsByName, userOptions) {
-
     const options = {
       ...defaultOptions,
       ...userOptions
     }
 
     const {
-      render
+      render,
+      mustProvideAllProps,
     } = options
+
+    if (mustProvideAllProps) {
+      if (typeof component === 'string') {
+        throw new Error('mustProvideAllProps option is not supported for built-in components')
+      }
+
+      const componentPropNames = Object.keys(component.propTypes)
+      const propNamesWithProvidedValues = Object.keys(possiblePropsByName)
+      const missingProps = componentPropNames.filter((pn) => propNamesWithProvidedValues.indexOf(pn) < 0);
+
+      if (missingProps.length) {
+        throw new Error(storyName + ': Missing possible values for props: ' + missingProps.join(', '))
+      }
+    }
 
     const propsCombinations = combinations(possiblePropsByName)
 
@@ -47,7 +62,6 @@ export default {
         {propsCombinations.map((props) => render(component, props, options))}
       </div>
     ))
-
   }
 }
 
