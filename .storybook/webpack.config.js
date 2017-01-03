601813baba43866521241eb80491758a8d9b9330
Valentin Agachi <github-com@agachi.name>
2017-01-03 22:37:05 +0000

    Fix missing children when used with preact-compat

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+// To test using preact, uncomment the following block
+/*
+  resolve: {
+		alias: {
+			react: 'preact-compat',
+			'react-dom': 'preact-compat',
+		},
+	},
+*/
+};
